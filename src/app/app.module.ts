d3107ac180b4aeb38bcc3e1ef8e569f8e9f91dea
Edgar Jegorov <jegorov.e@gmail.com>
2022-01-23 17:24:56 +0200

    render barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EffectsModule } from "@ngrx/effects";
 import { StoreModule } from "@ngrx/store";
 import { AppRoutingModule } from "@spartacus/storefront";
 import { AppComponent } from './app.component';
+import { ProductIntroModule } from "./product-intro/product-intro.module";
 import { SearchBoxModule } from "./search-box/search-box.module";
 import { SpartacusModule } from './spartacus/spartacus.module';
 
@@ -20,7 +21,8 @@ import { SpartacusModule } from './spartacus/spartacus.module';
     EffectsModule.forRoot([]),
     SpartacusModule,
     BrowserTransferStateModule,
-    SearchBoxModule
+    SearchBoxModule,
+    ProductIntroModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
